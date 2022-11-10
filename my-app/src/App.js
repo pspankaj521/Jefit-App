2faabb2044be0ff28571cea2cecb252338d399a3
pspankaj521 <pankaj.singh2151994@gmail.com>
2022-11-10 17:56:38 +0530

    2nd day

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/navbar';
+import BlueBox from './components/home_blue_sec';
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      <BlueBox/>
     </div>
   );
 }
--- my-app/src/components/home_blue_sec.jsx
+++ my-app/src/components/home_blue_sec.jsx
@@ -1,11 +1,46 @@
-function BlueBox(){
-    return(
-  <div >
-    <div></div>
-    <div>
+import {
+  Flex,
+  Spacer,
+  Center,
+  Square,
+  Text,
+  Box,
+  Image,
+  Heading,
+  HStack,
+  Grid,
+  GridItem
+} from "@chakra-ui/react";
 
+function BlueBox() {
+  return (
+    <div style={{ backgroundColor: "#003c8f" }}>
+      <Flex color="white">
+        <Center w="55%" h="800px">
+          <Text
+            fontSize="6xl"
+            fontWeight="bold"
+            textAlign="left"
+            marginTop="-350px"
+            w="90%"
+          >
+            MANAGE & TRACK ALL YOUR WORKOUTS IN ONE PLACE
+          </Text>
+          {/* <br/>
+          <Text  fontSize="3xl" fontWeight="bold" >#1 Popular Workout Tracking Platform Stay Strong Together</Text> */}
+         
+        </Center>
+       
+
+        <Square w="45%" h="800px">
+          <Image
+            w="120%"
+            src="https://www.jefit.com/wp/wp-content/uploads/2022/03/website_hero_watch-1536x1280.png"
+            alt=""
+          />
+        </Square>
+      </Flex>
     </div>
-  </div>
-
-    )
+  );
 }
+export default BlueBox;
